50e6b95531a21c22ef950dc248562a83299fe5da
HOME <kongyaa89@gmail.com>
2018-07-30 22:08:13 +0900

    chapter 1 : what is JSX

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
+
+    // var = 함수단위, const, let = 블록단위 scope
+    const name = 'react';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {/* 주석은 이렇게 */}
+        <div>
+            hello {name}!
+        </div>
+        {/* 삼항 연산자 */}
+        {
+          1 + 1 !== 2
+            ? (<div> 맞아요!</div>)
+            : (<div> 틀려요!</div>)
+        }
+        {/* AND 연산자 */}
+        {
+          1+1 === 2 && (<div>맞아요!</div>)
+        }
+        {/* IF 문 */}
+        {
+          (function() {
+            if (value === 1) return (<div>하나</div>);
+            if (value === 2) return (<div>둘</div>);
+            if (value === 3) return (<div>셋</div>);
+          })()
+        }
       </div>
     );
   }
 }
+
+
 
 export default App;
